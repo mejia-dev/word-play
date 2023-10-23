1f67e1d750b9655782f9cdbcfc24692371879c0c
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-23 14:36:40 -0700

    Add basic structure with pseudocode and write out reverseAndDisplay function
    Co-authored-by: Ben Reynolds <bgraynolds@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+String.prototype.split()
+
+function convertToArray() {
+  // get input
+  // split into array
+  // find any array element that is longer than 3 characters
+  // add results to newArray
+  // call reverseAndDisplay(newArray)
+
+words.forEach(word =>3 {
+    wordLengths.push;
+}
+
+}
+
+function reverseAndDisplay(input) {
+  let reversedArray = input.reverse();
+  const resultDiv = document.getElementById("result");
+  const resultText = document.createElement("p");
+  resultText.append(reversedArray);
+  resultDiv.append(resultText);
+};
+
+
+
+window.addEventListener("load", function() {
+  this.document.getElementById("questionInput").addEventListener("submit", convertToArray);
+});
